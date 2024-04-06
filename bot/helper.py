8aa2ebd72950e010db5f61a38648439ac7a50c1b
NinjaO9 <davidmontesvilanova@gmail.com>
2024-04-06 15:50:04 -0800

    Followed a JS tutorial. Experiment to see if I can use this for discord bot

---
+++ bot/helper.py
@@ -0,0 +1,75 @@
+# Python file for all the "background tasks" such as verifying and sorting lists
+import bot.main as main, datetime
+from datetime import date
+
+bot = main.bot
+blacklist = [] #blacklist of news that should no longer appear on messages sent by the bot.
+
+def getArticles(keywords,count,time,sortBy):
+    global blacklist
+    articles = main.newsapi.get_everything(q=(f"{keywords}"), language="en", from_param=time, sort_by=sortBy)["articles"]
+    articles = list(filter( lambda item: item not in blacklist, articles))
+    articles = articles[:int(verifyCountIntegrity(count))]
+    blacklist = blacklist + articles
+    return articles
+
+def verifyCountIntegrity(count: str):
+    if count == None:
+        return 1
+    if count.isdigit():
+        return count
+    else:
+        print(f"{count} isn't a number")
+        return 1
+
+def verifyChannelIntegrity(channel:str):
+    if channel == None:
+        return None
+    if channel.isdigit():
+        return channel
+    else:
+        return None
+
+def verifyDateIntegrity(time:str):
+    datechar = list(time)
+    temp = ''
+    datechar.pop[4]
+    datechar.pop[6] #pop where the "-" should be in the dates "####-##-##"
+    temp = str(datechar)
+    if temp.isdigit():
+        return time
+    else:
+        return getYesterday()
+    
+def getYesterday():
+    day = date.today()
+    return (day - datetime.timedelta(days=1))
+
+
+def formatNews(news):
+    message = ''
+    for article in news:
+            if len(message) > 1500:
+                return message
+            message = message + (f"{article['title']}: \n {article['url']} \n")
+    return message
+
+def getActiveChannel(channel):
+    global active_channel
+    try:
+        active_channel = bot.get_channel(int(verifyChannelIntegrity(channel)))
+        return active_channel
+    except Exception as e:
+        print("No channel ID, or invalid channel ID was given")
+
+def setGlobalActiveChannel(channel):
+    global active_channel
+    active_channel = channel
+
+def getGlobalActiveChannel():
+    global active_channel
+    try:
+        return active_channel
+    except Exception as e:
+        if active_channel != None:
+            print(f"No active channel set? {e}")
---
+++ bot/main.py
@@ -0,0 +1,45 @@
+import nextcord, os, bot.getkeys as getkeys
+from nextcord.ext import commands
+from newsapi import NewsApiClient
+from dotenv import load_dotenv
+
+# Init bot
+load_dotenv()
+intents = nextcord.Intents.default()
+bot = commands.Bot(intents=intents)
+
+# Set keys and stuff
+newsapi = NewsApiClient(api_key=os.getenv('API_KEY')) 
+bottoken = (os.getenv('BOT_TOKEN')) 
+serverid = (getkeys.key_list[0])
+
+# Set up slash commands
+command_list = []
+
+for file in os.listdir("./slash"):
+    if file.endswith(".py"):
+        command_list.append("slash." + file[:-3])
+
+#Incase something is missing upon installation
+if newsapi == None or bottoken == None or serverid == None:
+    print("You're missing an initilizing value! Check the 'keys.txt' file!")
+
+def getActiveChannelID(channel):
+    return bot.get_channel(channel)
+# Alert the user that the bot is ready
+@bot.event
+async def on_ready():
+    await bot.wait_until_ready()
+    print("---------------------------------")
+    print("         Bot is online!          ")
+    print("---------------------------------")
+
+
+for file in command_list:
+    print(f"loading '{file[6:]}' command...")
+    bot.load_extension(file)
+bot.run(token=bottoken)
+
+#-------------------------------------------------------
+# Envbot by NinjaO9 @https://github.com/NinjaO9/EnvBot
+#-------------------------------------------------------
